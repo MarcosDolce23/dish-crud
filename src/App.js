684c60a2af556953771adcbcc398efadd74ebb17
Marcos Dolce <marcosdolce.91@gmail.com>
2023-09-08 23:46:14 -0300

    indent components

--- src/App.js
+++ src/App.js
@@ -3,79 +3,81 @@ import React from "react";
 
 // Import Bootstrap
 import { Nav, Navbar, Container, Row, Col }
-		from "react-bootstrap";
+  from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 
 // Import Custom CSS
 import "./App.css";
 
 // Import from react-router-dom
-import { BrowserRouter as Router, Routes,
-	Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router, Routes,
+  Route, Link
+} from "react-router-dom";
 
 // Import other React Component
 import CreateDish from
-	"./Components/create-dish.component";
+  "./Components/create-dish.component";
 import EditDish from
-	"./Components/edit-dish.component";
+  "./Components/edit-dish.component";
 import DishList from
-	"./Components/dish-list.component";
+  "./Components/dish-list.component";
 
 // App Component
 const App = () => {
-return (
-	<Router>
-	<div className="App">
-		<header className="App-header">
-		<Navbar bg="dark" variant="dark">
-			<Container>
-			<Navbar.Brand>
-				<Link to={"/create-dish"}
-				className="nav-link">
-				React MERN Stack App
-				</Link>
-			</Navbar.Brand>
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <Navbar.Brand>
+                <Link to={"/create-dish"}
+                  className="nav-link">
+                  React MERN Stack App
+                </Link>
+              </Navbar.Brand>
 
-			<Nav className="justify-content-end">
-				<Nav>
-				<Link to={"/create-dish"}
-					className="nav-link">
-					Create Dish
-				</Link>
-				</Nav>
+              <Nav className="justify-content-end">
+                <Nav>
+                  <Link to={"/create-dish"}
+                    className="nav-link">
+                    Create Dish
+                  </Link>
+                </Nav>
 
-				<Nav>
-				<Link to={"/dish-list"}
-					className="nav-link">
-					Dish List
-				</Link>
-				</Nav>
-			</Nav>
-			</Container>
-		</Navbar>
-		</header>
+                <Nav>
+                  <Link to={"/dish-list"}
+                    className="nav-link">
+                    Dish List
+                  </Link>
+                </Nav>
+              </Nav>
+            </Container>
+          </Navbar>
+        </header>
 
-		<Container>
-		<Row>
-			<Col md={12}>
-			<div className="wrapper">
-				<Routes>
-				<Route exact path="/"
-					element={<CreateDish/>} />
-				<Route path="/create-dish"
-					element={<CreateDish/>} />
-				<Route path="/edit-dish/:id"
-					element={<EditDish/>} />
-				<Route path="/dish-list"
-					element={<DishList/>} />
-				</Routes>
-			</div>
-			</Col>
-		</Row>
-		</Container>
-	</div>
-	</Router>
-);
+        <Container>
+          <Row>
+            <Col md={12}>
+              <div className="wrapper">
+                <Routes>
+                  <Route exact path="/"
+                    element={<CreateDish />} />
+                  <Route path="/create-dish"
+                    element={<CreateDish />} />
+                  <Route path="/edit-dish/:id"
+                    element={<EditDish />} />
+                  <Route path="/dish-list"
+                    element={<DishList />} />
+                </Routes>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
--- src/Components/dish-list.component.js
+++ src/Components/dish-list.component.js
@@ -4,40 +4,40 @@ import { Table } from "react-bootstrap";
 import DishTableRow from "./DishTableRow";
 
 const DishList = () => {
-const [dishes, setDishes] = useState([]);
+    const [dishes, setDishes] = useState([]);
 
-useEffect(() => {
-	axios
-	.get("http://localhost:4000/dishes/")
-	.then(({ data }) => {
-		setDishes(data);
-	})
-	.catch((error) => {
-		console.log(error);
-	});
-}, []);
+    useEffect(() => {
+        axios
+            .get("http://localhost:4000/dishes/")
+            .then(({ data }) => {
+                setDishes(data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
 
-const DataTable = () => {
-	return dishes.map((res, i) => {
-	return <DishTableRow obj={res} key={i} />;
-	});
-};
+    const DataTable = () => {
+        return dishes.map((res, i) => {
+            return <DishTableRow obj={res} key={i} />;
+        });
+    };
 
-return (
-	<div className="table-wrapper">
-	<Table striped bordered hover>
-		<thead>
-		<tr>
-			<th>Name</th>
-			<th>Email</th>
-			<th>Roll No</th>
-			<th>Action</th>
-		</tr>
-		</thead>
-		<tbody>{DataTable()}</tbody>
-	</Table>
-	</div>
-);
+    return (
+        <div className="table-wrapper">
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Roll No</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>{DataTable()}</tbody>
+            </Table>
+        </div>
+    );
 };
 
 export default DishList;
