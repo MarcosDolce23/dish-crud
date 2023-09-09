95e69e9b9ad6eeadfdd64a07e4e6a945411f64ac
Marcos Dolce <marcosdolce.91@gmail.com>
2023-09-09 01:20:23 -0300

    added bootstrap to createdish

--- src/Components/DishForm.js
+++ src/Components/DishForm.js
@@ -1,135 +1,139 @@
 import React from "react";
+import { useState } from "react";
 import * as Yup from "yup";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import { FormGroup, FormControl, Button } from "react-bootstrap";
+import { Form, Row, Col, InputGroup, Button } from "react-bootstrap";
 
 const DishForm = (props) => {
-    const validationSchema = Yup.object().shape({
-        esName: Yup.string().required("Required"),
-        enName: Yup.string().required("Required"),
-        esLabel: Yup.string().required("Required"),
-        enLabel: Yup.string().required("Required"),
-        cookTime: Yup.number()
-            .positive("Invalid roll number")
-            .integer("Invalid roll number")
-            .required("Required"),
-        vegan: Yup.boolean().required("Required"),
-        esIngredients: Yup.array().required("Required"),
-        enIngredients: Yup.array().required("Required"),
-        esRecipes: Yup.array().required("Required"),
-        enRecipes: Yup.array().required("Required")
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        setValidated(true);
+    };
+
+    const Ingredients = Array(24).fill(null).map( e => {
+        return (
+            <Form.Group as={Col} xs="4" md="1" controlId="validationCustom01">
+                <Form.Check
+                    label="Vegan"
+                />
+            </Form.Group>
+        )
     });
-    console.log(props);
+
     return (
-        <div className="form-wrapper">
-            <Formik {...props} validationSchema={validationSchema}>
-                <Form>
-                    <FormGroup>
-                        <label htmlFor="esName">ES Name:</label>
-                        <Field id="esName" name="esName" type="text"
-                            className="form-control" />
-                        <ErrorMessage
-                            name="esName"
-                            className="d-block invalid-feedback"
-                            component="span"
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                        <label htmlFor="enName">EN Name:</label>
-                        <Field id="enName" name="enName" type="text"
-                            className="form-control" />
-                        <ErrorMessage
-                            name="enName"
-                            className="d-block invalid-feedback"
-                            component="span"
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                        <label htmlFor="esLabel">ES Label:</label>
-                        <Field id="esLabel" name="esLabel" type="text"
-                            className="form-control" />
-                        <ErrorMessage
-                            name="esLabel"
-                            className="d-block invalid-feedback"
-                            component="span"
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                        <label htmlFor="enLabel">EN Label:</label>
-                        <Field id="enLabel" name="enLabel" type="text"
-                            className="form-control" />
-                        <ErrorMessage
-                            name="enLabel"
-                            className="d-block invalid-feedback"
-                            component="span"
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                        <label htmlFor="cookTime">Cook time:</label>
-                        <Field id="cookTime" name="cookTime" type="number"
-                            className="form-control" />
-                        <ErrorMessage
-                            name="cookTime"
-                            className="d-block invalid-feedback"
-                            component="span"
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                        <label htmlFor="vegan">Vegan:</label>
-                        <Field id="vegan" name="vegan" type="checkbox" />
-                        <ErrorMessage
-                            name="vegan"
-                            className="d-block invalid-feedback"
-                            component="span"
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                        <label htmlFor="esIngredients">ES Ingredients:</label>
-                        <Field id="esIngredients" name="esIngredients" type="text"
-                            className="form-control" />
-                        <ErrorMessage
-                            name="esIngredients"
-                            className="d-block invalid-feedback"
-                            component="span"
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                        <label htmlFor="enIngredients">EN Ingredients:</label>
-                        <Field id="enIngredients" name="enIngredients" type="text"
-                            className="form-control" />
-                        <ErrorMessage
-                            name="enIngredients"
-                            className="d-block invalid-feedback"
-                            component="span"
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                        <label htmlFor="esRecipes">ES Recipes:</label>
-                        <Field id="esRecipes" name="esRecipes" type="text"
-                            className="form-control" />
-                        <ErrorMessage
-                            name="esRecipes"
-                            className="d-block invalid-feedback"
-                            component="span"
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                        <label htmlFor="enRecipes">EN Recipes:</label>
-                        <Field id="enRecipes" name="enRecipes" type="text"
-                            className="form-control" />
-                        <ErrorMessage
-                            name="enRecipes"
-                            className="d-block invalid-feedback"
-                            component="span"
-                        />
-                    </FormGroup>
-                    <Button variant="danger" size="lg"
-                        block="block" type="submit">
-                        {props.children}
-                    </Button>
-                </Form>
-            </Formik>
-        </div>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Row className="mb-3">
+                <Form.Group as={Col} md="3" controlId="validationCustom01">
+                    <Form.Label>ES Name</Form.Label>
+                    <Form.Control
+                        required
+                        type="text"
+                        placeholder="ES Name"
+                    />
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group as={Col} md="3" controlId="validationCustom02">
+                    <Form.Label>EN Name</Form.Label>
+                    <Form.Control
+                        required
+                        type="text"
+                        placeholder="EN Name"
+                    />
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                </Form.Group>
+            </Row>
+            <Row className="mb-3">
+                <Form.Group as={Col} md="3" controlId="validationCustom01">
+                    <Form.Label>ES Label</Form.Label>
+                    <Form.Control
+                        required
+                        type="text"
+                        placeholder="ES Label"
+                    />
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group as={Col} md="3" controlId="validationCustom02">
+                    <Form.Label>EN Label</Form.Label>
+                    <Form.Control
+                        required
+                        type="text"
+                        placeholder="EN Label"
+                    />
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                </Form.Group>
+            </Row>
+            <Row className="mb-3">
+                <Form.Group as={Col} md="3" controlId="validationCustom01">
+                    <Form.Label>Cook time</Form.Label>
+                    <Form.Control
+                        required
+                        type="number"
+                        placeholder="Cook time"
+                    />
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                </Form.Group>
+            </Row>
+            <Row className="mb-3">
+                <Form.Group as={Col} md="3" controlId="validationCustom01">
+                    <Form.Check
+                        label="Vegan"
+                    />
+                </Form.Group>
+            </Row>
+            <Row className="mb-3">
+                <Form.Label>Ingredients</Form.Label>
+                {Ingredients}
+            </Row>
+            <Row className="mb-3">
+                <Form.Group as={Col} md="8" controlId="validationCustom01">
+                    <Form.Label>ES Recipe</Form.Label>
+                    <Form.Control
+                        required
+                        as="textarea"
+                        placeholder="ES Recipe"
+                    />
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                </Form.Group>
+            </Row>
+            <Row className="mb-3">
+                <Form.Group as={Col} md="8" controlId="validationCustom01">
+                    <Form.Label>EN Recipe</Form.Label>
+                    <Form.Control
+                        required
+                        as="textarea"
+                        placeholder="EN Recipe"
+                    />
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                </Form.Group>
+            </Row>
+            <Row className="mb-3">
+                <Form.Group as={Col} md="8" controlId="validationCustom01">
+                    <Form.Label>Image</Form.Label>
+                    <Form.Control
+                        required
+                        type="file"
+                    />
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                </Form.Group>
+            </Row>
+            <Row className="mb-3">
+                <Form.Group as={Col} md="8" controlId="validationCustom01">
+                    <Form.Label>Header image</Form.Label>
+                    <Form.Control
+                        required
+                        type="file"
+                    />
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                </Form.Group>
+            </Row>
+            <Button xs="12" type="submit">Submit form</Button>
+        </Form>
     );
 };
 
