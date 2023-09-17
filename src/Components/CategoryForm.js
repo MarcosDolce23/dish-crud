1b934aa9a8b8571cb5a2de221ea9460f956e3cbb
Marcos Dolce <marcosdolce.91@gmail.com>
2023-09-17 19:40:52 -0300

    Completo form para categorias

--- src/Components/CategoryForm.js
+++ src/Components/CategoryForm.js
@@ -1,19 +1,36 @@
 import React from "react";
+import Axios from "axios";
 import { useState, useEffect } from "react";
-import { Form, Row, Col, Button } from "react-bootstrap";
+import { Form, Row, Col, Button, Spinner } from "react-bootstrap";
 import { saveAs } from "file-saver";
-import IngredientsDropdown from "./IngredientsDropdown";
+import CategoryIngredientsDropdown from "./CategoryIngredientsDropdown";
 import Utilities from "./Common/Utilities";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const CategoryForm = ({ initialValues, onSubmit }) => {
     const [formData, setFormData] = useState({ ...initialValues });
+    const [ingredients, setIngredients] = useState([]);
     const [validated, setValidated] = useState(false);
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
 
     useEffect(() => {
         initialValues.ingredients.map(ingredient => {
             return ingredient.listId = Utilities.getRandomInt();
         });
+
+        Axios({
+            url: "http://localhost:4000/ingredients/",
+        })
+            .then((response) => {
+                setIsLoaded(true);
+                setIngredients(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+                setIsLoaded(true);
+                setError(error);
+            });
 
         setFormData(initialValues);
     }, [initialValues])
@@ -78,85 +95,96 @@ const CategoryForm = ({ initialValues, onSubmit }) => {
         let file = Utilities.convertBase64ToFile(src, name);
         saveAs(file, name);
     }
-    const renderIngredients = formData.ingredients.map(ingredient => <IngredientsDropdown
+    const renderIngredients = formData.ingredients.map(ingredient => <CategoryIngredientsDropdown
         key={ingredient.listId}
         ingredients={ingredients}
-        categories={categories}
         ingredient={ingredient}
         updateIngredient={updateIngredient}
         removeIngredient={removeIngredient}
     />);
 
-    return (
-        <Form noValidate validated={validated} onSubmit={handleSubmit}>
-            <Row className="mb-3">
-                <Form.Group as={Col} md="3" controlId="validationCustom01">
-                    <Form.Label>ES Name</Form.Label>
-                    <Form.Control
-                        required
-                        type="text"
-                        placeholder="ES Name"
-                        value={formData.esName}
-                        onChange={e => setFormData({ ...formData, esName: e.target.value })}
-                    />
-                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group as={Col} md="3" controlId="validationCustom02">
-                    <Form.Label>EN Name</Form.Label>
-                    <Form.Control
-                        required
-                        type="text"
-                        placeholder="EN Name"
-                        value={formData.enName}
-                        onChange={e => setFormData({ ...formData, enName: e.target.value })}
-                    />
-                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                </Form.Group>
-            </Row>
-            <Row className="mb-3">
-                <Form.Label>Ingredients</Form.Label>
-            </Row>
-            {renderIngredients}
-            <Row className="mb-3">
-                <Col xs="12">
-                    <Button variant="success" onClick={addIngredient}>Add Ingredient</Button>
-                </Col>
-            </Row>
-            <Form.Label>Image</Form.Label>
-            <Row className="mb-3">
-                <Form.Group as={Col} md="4" controlId="validationCustom09">
-                    <Form.Control
-                        type="file"
-                        onChange={e => handleImage(e)}
-                    />
-                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                </Form.Group>
-                {formData.image ? (
-                    <>
-                        <Col md="2">
-                            <div className="d-grid gap-2">
-                                <Button
-                                    variant="success"
-                                    onClick={() => openImage(formData.base64Image)}
-                                >See Image</Button>
-                            </div>
-                        </Col>
-                        <Col md="2">
-                            <div className="d-grid gap-2">
-                                <Button
-                                    variant="success"
-                                    onClick={() => downloadImage(formData.base64Header, formData.headerImage)}
-                                >Download</Button>
-                            </div>
-                        </Col>
-                    </>
-                ) : (
-                    null
-                )}
-            </Row>
-            <Button xs="12" type="submit">Submit form</Button>
-        </Form>
-    );
+    if (error) {
+        return <div>Error: {error.message}</div>
+    } else if (!isLoaded) {
+        return (
+            <div id="spinner">
+                <Spinner animation="border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>
+            </div>
+        )
+    } else {
+        return (
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                <Row className="mb-3">
+                    <Form.Group as={Col} md="3" controlId="validationCustom01">
+                        <Form.Label>ES Name</Form.Label>
+                        <Form.Control
+                            required
+                            type="text"
+                            placeholder="ES Name"
+                            value={formData.esName}
+                            onChange={e => setFormData({ ...formData, esName: e.target.value })}
+                        />
+                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group as={Col} md="3" controlId="validationCustom02">
+                        <Form.Label>EN Name</Form.Label>
+                        <Form.Control
+                            required
+                            type="text"
+                            placeholder="EN Name"
+                            value={formData.enName}
+                            onChange={e => setFormData({ ...formData, enName: e.target.value })}
+                        />
+                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                    </Form.Group>
+                </Row>
+                <Row className="mb-3">
+                    <Form.Label>Ingredients</Form.Label>
+                </Row>
+                {renderIngredients}
+                <Row className="mb-3">
+                    <Col xs="12">
+                        <Button variant="success" onClick={addIngredient}>Add Ingredient</Button>
+                    </Col>
+                </Row>
+                <Form.Label>Image</Form.Label>
+                <Row className="mb-3">
+                    <Form.Group as={Col} md="4" controlId="validationCustom09">
+                        <Form.Control
+                            type="file"
+                            onChange={e => handleImage(e)}
+                        />
+                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                    </Form.Group>
+                    {formData.image ? (
+                        <>
+                            <Col md="2">
+                                <div className="d-grid gap-2">
+                                    <Button
+                                        variant="success"
+                                        onClick={() => openImage(formData.base64Image)}
+                                    >See Image</Button>
+                                </div>
+                            </Col>
+                            <Col md="2">
+                                <div className="d-grid gap-2">
+                                    <Button
+                                        variant="success"
+                                        onClick={() => downloadImage(formData.base64Header, formData.headerImage)}
+                                    >Download</Button>
+                                </div>
+                            </Col>
+                        </>
+                    ) : (
+                        null
+                    )}
+                </Row>
+                <Button xs="12" type="submit">Submit form</Button>
+            </Form>
+        );
+    }
 };
 
 export default CategoryForm;
---
+++ src/Components/CategoryIngredientsDropdown.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { Form, Row, Col, CloseButton } from "react-bootstrap";
+
+const CategoryIngredientsDropdown = ({ ingredients, ingredient, updateIngredient, removeIngredient }) => {
+
+    const handleIngredientSelect = (e) => {
+        const ingId = e.target.value;
+        const selected = ingredients.filter(e => e._id === ingId);
+        updateIngredient({ ...ingredient, id: ingId, esName: selected[0].esName, enName: selected[0].enName });
+    };
+
+    const ingredientList = ingredients.map(ingredient => {
+        return (
+            <option key={ingredient._id} value={ingredient._id}>{ingredient.esName} | {ingredient.enName}</option>
+        )
+    });
+
+    return (
+        <Row className="mb-3">
+            <Form.Group as={Col} xs="4" md="3" controlId="validationCustomIngre">
+                <Form.Select value={ingredient._id} onChange={(e) => handleIngredientSelect(e)}>
+                    <option>Select an ingredient</option>
+                    {ingredientList}
+                </Form.Select>
+            </Form.Group>
+            <Form.Group as={Col} xs="4" md="3">
+                <CloseButton onClick={() => removeIngredient(ingredient)} />
+            </Form.Group>
+        </Row>
+    );
+}
+
+export default CategoryIngredientsDropdown;
