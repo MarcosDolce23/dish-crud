079e7832676914e407dd9a4bc2a48e5e44c2f466
Marcos Dolce <marcosdolce.91@gmail.com>
2023-09-17 15:18:14 -0300

    Creo IngredientsForm

---
+++ src/Components/IngredientForm.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Form, Row, Col, Button } from "react-bootstrap";
+import Utilities from "./Common/Utilities";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const IngredientForm = ({ initialValues, onSubmit }) => {
+    const [formData, setFormData] = useState({ ...initialValues });
+    const [validated, setValidated] = useState(false);
+
+    useEffect(() => {
+        setFormData(initialValues);
+    }, [initialValues])
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        setValidated(true);
+        onSubmit(formData);
+    };
+
+    return (
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Row className="mb-3">
+                <Form.Group as={Col} md="3" controlId="validationCustom01">
+                    <Form.Label>ES Name</Form.Label>
+                    <Form.Control
+                        required
+                        type="text"
+                        placeholder="ES Name"
+                        value={formData.esName}
+                        onChange={e => setFormData({ ...formData, esName: e.target.value })}
+                    />
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group as={Col} md="3" controlId="validationCustom02">
+                    <Form.Label>EN Name</Form.Label>
+                    <Form.Control
+                        required
+                        type="text"
+                        placeholder="EN Name"
+                        value={formData.enName}
+                        onChange={e => setFormData({ ...formData, enName: e.target.value })}
+                    />
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                </Form.Group>
+            </Row>
+            <Button xs="12" type="submit">Submit form</Button>
+        </Form>
+    );
+};
+
+export default IngredientForm;
