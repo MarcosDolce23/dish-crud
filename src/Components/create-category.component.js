d7ec6bf77b4a5f075be0fbb4a60ca0f7a8352d8e
Marcos Dolce <marcosdolce.91@gmail.com>
2023-09-21 20:00:55 -0300

    Modal para crear category

--- src/Components/create-category.component.js
+++ src/Components/create-category.component.js
@@ -1,8 +1,15 @@
 // CreateCategory Component for add new category
 import Axios from 'axios';
 import CategoryForm from './CategoryForm';
+import CommonModal from './Common/CommonModal';
+import { useState } from 'react';
 
 const CreateCategory = () => {
+    const [modalShow, setModalShow] = useState(false);
+    const [title, setTitle] = useState('');
+    const [subTitle, setSubTitle] = useState('');
+    const [text, setText] = useState('');
+
     const formData = {
         esName: '',
         enName: '',
@@ -21,20 +28,37 @@ const CreateCategory = () => {
         Axios.post(
             'http://localhost:4000/categories', payload)
             .then(res => {
-                if (res.status === 200)
-                    alert('Category successfully created')
-                else
+                if (res.status === 200) {
+                    setTitle('Successful!');
+                    setSubTitle('Category successfully created');
+                    setText('The category ' + res.data.esName + ' | ' + res.data.enName + ' was succesfully created');
+                    setModalShow(true);
+                } else
                     Promise.reject()
             })
-            .catch(err => console.log('Error: ' + err))
+            .catch(err => {
+                setTitle('Error!');
+                setSubTitle('Category not created');
+                setText('The category was not created: ' + err);
+                setModalShow(true);
+            })
     }
 
     return (
-        <CategoryForm initialValues={formData}
-            onSubmit={addCategory}
-        >
-            Create category 
-        </CategoryForm>
+        <>
+            <CategoryForm initialValues={formData}
+                onSubmit={addCategory}
+            >
+                Create category
+            </CategoryForm>
+            <CommonModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+                title={title}
+                subTitle={subTitle}
+                text={text}
+            />
+        </>
     )
 }
 
